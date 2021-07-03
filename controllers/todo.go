4631802a939ec24a851a5405644694e540f6f6cc
ec965 <enoch965@gmail.com>
2021-07-02 20:32:14 -0700

    loading...

--- controllers/todo.go
+++ controllers/todo.go
@@ -1,21 +1,50 @@
 package controllers
 
 import (
+	"strconv"
+
 	"github.com/ec965/todo-api/models"
 	"github.com/gin-gonic/gin"
-	"strconv"
 )
+
+// check if the form has the value
+func formHasValue(c *gin.Context, key string) string {
+	field := c.PostForm(key)
+	if field == "" {
+		c.JSON(400, gin.H{"error": key + " is required"})
+		return ""
+	}
+	return field
+}
+
+// check if the url has the query param
+func queryParamHasValue(c *gin.Context, key string) string {
+	field := c.Query(key)
+	if field == "" {
+		c.JSON(400, gin.H{"error": key + " is required"})
+		return ""
+	}
+	return field
+}
+
+// handle database errors
+func handleError(c *gin.Context, err error) bool {
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return true
+	}
+	return false
+}
 
 // Create a new todo
 func CreateTodo(c *gin.Context) {
-	title := c.PostForm("title")
+	title := formHasValue(c, "title")
 	if title == "" {
-		c.JSON(400, gin.H{"error": "title is required"})
 		return
 	}
 	todo, err := models.CreateTodo(title)
-	if err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+	if handleError(c, err) {
+		return
 	}
 	c.JSON(200, todo)
 }
@@ -23,17 +52,16 @@ func CreateTodo(c *gin.Context) {
 // Get all todos
 func GetTodos(c *gin.Context) {
 	todos, err := models.GetAllTodo()
-	if err != nil {
-		c.JSON(400, gin.H{"error": err.Error()})
+	if handleError(c, err) {
+		return
 	}
 	c.JSON(200, todos)
 }
 
 // get a single todo by id query parameter
 func GetTodo(c *gin.Context) {
-	idStr := c.Query("id")
+	idStr := queryParamHasValue(c, "id")
 	if idStr == "" {
-		c.JSON(400, gin.H{"error": "id is required"})
 		return
 	}
 	id64, err := strconv.ParseUint(idStr, 10, 32)
@@ -48,4 +76,26 @@ func GetTodo(c *gin.Context) {
 		return
 	}
 	c.JSON(200, todo)
+}
+
+func UpdateTodo(c *gin.Context) {
+	idStr := formHasValue(c, "id")
+	if idStr == "" {
+		return
+	}
+	id64, err := strconv.ParseUint(idStr, 10, 32)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "id is not a number"})
+		return
+	}
+	id := uint(id64)
+	todo, err := models.GetTodoById(id)
+	if handleError(c, err) {
+		return
+	}
+	updatedTodo, err := models.UpdateTodo(id, todo.Title, true)
+	if handleError(c, err) {
+		return
+	}
+	c.JSON(200, updatedTodo)
 }
--- models/todo.go
+++ models/todo.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Todo struct {
 	gorm.Model
-	ID    uint 
+	ID    uint
 	Title string
 	Done  bool
 }
@@ -13,21 +13,21 @@ func CreateTodo(title string) (*Todo, error) {
 	todo := Todo{Title: title, Done: false}
 	result := db.Create(&todo)
 	if result.Error != nil {
-		return nil, result.Error 
+		return nil, result.Error
 	}
 	return &todo, nil
 }
 
-func GetTodoById(id uint)(*Todo, error) {
+func GetTodoById(id uint) (*Todo, error) {
 	todo := Todo{ID: id}
 	result := db.First(&todo)
 	if result.Error != nil {
-		return nil, result.Error 
+		return nil, result.Error
 	}
 	return &todo, nil
 }
 
-func GetAllTodo()([]*Todo, error) {
+func GetAllTodo() ([]*Todo, error) {
 	todos := []*Todo{}
 	db.Find(&todos)
 	return todos, nil
@@ -37,16 +37,16 @@ func DeleteTodo(id uint) error {
 	todo := Todo{ID: id}
 	result := db.Delete(&todo)
 	if result.Error != nil {
-		return result.Error 
+		return result.Error
 	}
 	return nil
 }
 
-func UpdateTodo(id uint, title string, done bool) error {
+func UpdateTodo(id uint, title string, done bool) (*Todo, error) {
 	todo := Todo{ID: id, Title: title, Done: done}
 	result := db.Save(&todo)
 	if result.Error != nil {
-		return result.Error 
+		return nil, result.Error
 	}
-	return nil
+	return &todo, nil
 }
